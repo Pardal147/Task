66f7d4822765d647a688d27308fc38ad6dc38d92
Pardal147 <108307927+Pardal147@users.noreply.github.com>
2023-01-13 18:27:49 -0300

    adicionado checkBox

--- index.js
+++ index.js
@@ -1,28 +1,58 @@
 
 let textinput = document.getElementById('textValue')
 let inputBtn = document.getElementById('inputBtn')
+let delet = document.getElementById('apgar')
+
+delet.onclick = function(){
+
+    let x = document.getElementById('lu')
+    x.textContent = ''
+}
 
 textinput.addEventListener('keypress',(teclado)=>{
 
     let lu = document.getElementById('lu')
     let li = document.createElement('li')
     let btn = document.createElement('button')
+    let check = document.createElement('input')
+   
+    if(teclado.keyCode === 13 ){
 
-    if(teclado.keyCode === 13){ 
+        check.setAttribute("type","checkbox") // dando um type e qual type ta recebendo
+        li.appendChild(check) // checkBox dentro do lu ou li n sei qual botar
 
-        li.textContent = textinput.value
-        lu.appendChild(li)
-        btn.textContent = '-'
-        li.appendChild(btn)
+        li.textContent = textinput.value // valor do input
+        lu.appendChild(li) //li dentro do lu
 
-        textinput.value = ''
+        btn.textContent = '-' //texto dentro do botao
+        li.appendChild(btn) //botar button dentro do li que foi criado
+
+        textinput.value = '' //limpar o input
     }
 
+    // document.getElementById('null').innerHTML = 'Escreva algo'
+
     btn.onclick = () =>{
-        console.log(li.textContent)
-        li.textContent = ''
+        lu.removeChild(li)
     }
 })
 
+// inputBtn.onclick =() =>{
+    
+//     let lu = document.getElementById('lu')
+//     let li = document.createElement('li')
+//     let btn = document.createElement('button')
 
+//     textinput.focus()
+//     li.textContent = textinput.value
+//     lu.appendChild(li)
+//     btn.textContent = '-'
+//     li.appendChild(btn)
+//     textinput.value = ''
+//     document.getElementById('null').innerHTML = ''
+
+//     btn.onclick = () =>{
+//         lu.removeChild(li)
+//     }
+// }
 
