f5fa4bdae3a970f4034cae58286123213509f13b
Pardal147 <108307927+Pardal147@users.noreply.github.com>
2023-01-13 12:23:56 -0300

    pressiona enter e aciona li e btn

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 
 let textinput = document.getElementById('textValue')
+let inputBtn = document.getElementById('inputBtn')
 
 textinput.addEventListener('keypress',(teclado)=>{
-    // let lugar = document.getElementById('lugar')
 
-    if(textinput.value !== undefined && teclado.keyCode === 13){ 
-        lugar.innerHTML += textinput.value
-        console.log(textinput.value)
+    let lu = document.getElementById('lu')
+    let li = document.createElement('li')
+    let btn = document.createElement('button')
+
+    if(teclado.keyCode === 13){ 
+
+        li.textContent = textinput.value
+        lu.appendChild(li)
+        btn.textContent = '-'
+        li.appendChild(btn)
+
         textinput.value = ''
     }
+
+    btn.onclick = () =>{
+        console.log(li.textContent)
+        li.textContent = ''
+    }
 })
+
+
 
