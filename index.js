de8d6e771782b326d78ad187d16f16e5f2c4bcf0
Pardal147 <108307927+Pardal147@users.noreply.github.com>
2023-01-14 09:28:13 -0300

    campo de texto vazio funcionado com enter e click em +

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ let textinput = document.getElementById('textValue')
 let inputBtn = document.getElementById('inputBtn')
 let delet = document.getElementById('apgar')
 
-delet.onclick = function(){
+delet.onclick =()=>{
 
     let x = document.getElementById('lu')
+    // let y = document.createElement('input')
     x.textContent = ''
 }
 
@@ -14,45 +15,61 @@ textinput.addEventListener('keypress',(teclado)=>{
     let lu = document.getElementById('lu')
     let li = document.createElement('li')
     let btn = document.createElement('button')
-    let check = document.createElement('input')
-   
-    if(teclado.keyCode === 13 ){
+    let checkBox = document.createElement('input')
 
-        check.setAttribute("type","checkbox") // dando um type e qual type ta recebendo
-        li.appendChild(check) // checkBox dentro do lu ou li n sei qual botar
+    if(teclado.keyCode === 13 && textinput.value !== ""){
 
         li.textContent = textinput.value // valor do input
         lu.appendChild(li) //li dentro do lu
 
+        checkBox.setAttribute("type","checkbox") // dando um type e qual type ta recebendo
+        li.appendChild(checkBox) // checkBox dentro do lu ou li n sei qual botar
+
         btn.textContent = '-' //texto dentro do botao
         li.appendChild(btn) //botar button dentro do li que foi criado
 
+        // let selectChecBox = document.querySelector(type="checkbox")
+        // console.log( document.querySelector(type="checkbox"))
+
+        document.getElementById('null').innerHTML = ''
         textinput.value = '' //limpar o input
     }
 
-    // document.getElementById('null').innerHTML = 'Escreva algo'
-
+    else if(teclado.keyCode === 13 && textinput.value === ""){
+        document.getElementById('null').innerHTML = 'Adicione algo a lista'
+    }
+    
     btn.onclick = () =>{
         lu.removeChild(li)
     }
-})
+}) 
 
-// inputBtn.onclick =() =>{
+inputBtn.onclick=()=>{
+
+    let lu = document.getElementById('lu')
+    let li = document.createElement('li')
+    let btn = document.createElement('button')
+    let checkBox = document.createElement('input')
     
-//     let lu = document.getElementById('lu')
-//     let li = document.createElement('li')
-//     let btn = document.createElement('button')
-
-//     textinput.focus()
-//     li.textContent = textinput.value
-//     lu.appendChild(li)
-//     btn.textContent = '-'
-//     li.appendChild(btn)
-//     textinput.value = ''
-//     document.getElementById('null').innerHTML = ''
-
-//     btn.onclick = () =>{
-//         lu.removeChild(li)
-//     }
-// }
-
+    if(textinput.value === ""){
+        document.getElementById('null').innerHTML = 'Adicione algo a lista'
+    }
+    else{ 
+        textinput.focus()
+        li.textContent = textinput.value // valor do input
+        lu.appendChild(li) //li dentro do lu
+    
+        checkBox.setAttribute("type","checkbox") // dando um type e o valor type ta recebendo
+        li.appendChild(checkBox) // checkBox dentro do lu ou li n sei qual botar
+    
+        btn.textContent = '-' //texto dentro do botao
+        li.appendChild(btn) //botar button dentro do li que foi criado
+    
+        document.getElementById('null').innerHTML = ''
+        textinput.value = '' //limpar o input
+    }
+  
+    btn.onclick = () =>{
+        lu.removeChild(li)
+    }
+}
